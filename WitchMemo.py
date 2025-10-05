dfedf9bf1bf531ee896bc3d6dbb51134e0315fd2
ProgramBio <Programgramprobio@gmail.com>
2025-10-06 04:02:04 +0700

    some music add

--- WitchMemo.py
+++ WitchMemo.py
@@ -6,6 +6,9 @@ pygame.init()
 SCREEN = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Menu")
 
+computer_name = socket.gethostname()
+user_name = getpass.getuser()
+
 BG = pygame.image.load("Image/Background/menubackgroundtest.jpg")
 basecolor = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) #สุ่มสีเริ่มต้น
 triadic_2 = [basecolor[0] + 85, basecolor[1] + 85, basecolor[2] + 85] #การทำสีที่สองของ Triadic Theory
@@ -24,11 +27,11 @@ def play():
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
         SCREEN.fill(basecolor)
-        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, triadic_2)
+        PLAY_TEXT = get_font(45).render("Hey " + user_name + ", buddy it's not time to play.", True, triadic_2)
         PLAY_RECT = PLAY_TEXT.get_rect(center = (640, 260))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
 
-        PLAY_BACK = Button(image = None, pos = (640, 460), text_input = "BACK", font = get_font(75), base_color = basecolor, hovering_color = triadic_2)
+        PLAY_BACK = Button(image = None, pos = (640, 460), text_input = "BACK", font = get_font(75), base_color = triadic_2, hovering_color = triadic_3)
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(SCREEN)
 
@@ -44,14 +47,14 @@ def options():
     while True:
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN.fill("white")
+        SCREEN.fill(basecolor)
 
-        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_TEXT = get_font(45).render("You thing I had made this? You fool.", True, triadic_2)
         OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
         OPTIONS_BACK = Button(image=None, pos=(640, 460), 
-                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+                            text_input="BACK", font=get_font(75), base_color = triadic_2, hovering_color = triadic_3)
 
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(SCREEN)
@@ -72,22 +75,23 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render("MAIN MENU", True, basecolor)
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, triadic_2)
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
         PLAY_BUTTON = Button(image=pygame.image.load("Image/Play Rect.png"), pos=(640, 250), 
-                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color = triadic_3)
         OPTIONS_BUTTON = Button(image=pygame.image.load("Image/Options Rect.png"), pos=(640, 400), 
-                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color = triadic_3)
         QUIT_BUTTON = Button(image=pygame.image.load("Image/Quit Rect.png"), pos=(640, 550), 
-                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color = triadic_3)
+        
         SCREEN.blit(MENU_TEXT, MENU_RECT)
-
         for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
-        
+            pygame.mixer_music.load("Music/003. Your Best Friend.mp3")
+            pygame.mixer.music.set_volume(0.25)
+            pygame.mixer.music.play(-1)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
